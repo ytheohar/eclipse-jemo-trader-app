4d7445d82e14457a37301862671c6f5f68f99beb
Christopher Stura <christopher.stura@cloudreach.com>
2019-04-01 10:25:23 +0100

    I makse sure the fixed process stops running when the stop method is called on the module.

--- src/main/java/org/eclipse/jemo/tutorial/market/MarketWatch.java
+++ src/main/java/org/eclipse/jemo/tutorial/market/MarketWatch.java
@@ -6,6 +6,7 @@ import org.eclipse.jemo.api.ModuleLimit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static java.util.logging.Level.INFO;
 
@@ -17,11 +18,22 @@ import static java.util.logging.Level.INFO;
 public class MarketWatch implements FixedModule {
 
     public static final List<Transaction> TRANSACTIONS = new ArrayList<>();
+    public AtomicBoolean running = new AtomicBoolean(false);
+
+    @Override
+    public void start() {
+        running.set(true);
+    }
+
+    @Override
+    public void stop() {
+        running.set(false);
+    }
 
     @Override
     public void processFixed(String location, String instanceId) throws Throwable {
         log(INFO, String.format("Process fixed Location [%s] - instance [%s]: [%s]", location, instanceId, TRANSACTIONS));
-        while (true) {
+        while (running.get()) {
             // We could send the transactions to a consumer. For demo purposes we just log them.
             TRANSACTIONS.forEach(txn -> log(INFO, String.format("Txn processed in Location [%s] - instance [%s]: [%s]", location, instanceId, txn)));
             TRANSACTIONS.clear();
